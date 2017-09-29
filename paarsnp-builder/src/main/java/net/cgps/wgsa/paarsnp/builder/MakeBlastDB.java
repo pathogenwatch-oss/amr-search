bc7ad61b7002ffc3b2f81045cefa3118b54a681f
CorinY <dr.yeats@gmail.com>
2017-09-29 11:23:52 +0100

    BLAST DB creator checks that the FASTA has content before running.

--- paarsnp-builder/src/main/java/net/cgps/wgsa/paarsnp/builder/MakeBlastDB.java
+++ paarsnp-builder/src/main/java/net/cgps/wgsa/paarsnp/builder/MakeBlastDB.java
@@ -5,9 +5,11 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.function.BiConsumer;
 
@@ -23,51 +25,67 @@ public class MakeBlastDB implements BiConsumer<String, Path> {
 
   @Override
   public void accept(final String libraryName, final Path fastaPath) {
-    final String[] cmd = {"makeblastdb",
-        "-dbtype",
-        "nucl",
-        "-parse_seqids",
-        "-in",
-        fastaPath.toAbsolutePath().toString(),
-        "-title",
-        libraryName,
-        "-out",
-        FileSystems.getDefault().getPath(this.outputDirectory.toString(), libraryName).toAbsolutePath().toString()};
 
-    final ProcessBuilder pb = new ProcessBuilder(cmd);
+    final boolean fastaIsEmpty;
 
-    this.logger.info("Prepared command: {}", StringUtils.join(cmd, " "));
+    try (final BufferedReader br = Files.newBufferedReader(fastaPath)) {
 
-    final Process p;
-
-    try {
-      p = pb.start();
-    } catch (final IOException e) {
-      throw new RuntimeException(e);
-    }
-
-    this.logger.debug("Started running library build.");
-    // Start the process
-    try (final InputStream error = p.getErrorStream(); final InputStream stdout = p.getInputStream()) {
-
-      // create listener for the stderr on separate thread (reports stderr to log).
-      final Thread errorGobbler = new StreamGobbler(error, "ERROR");
-      errorGobbler.start();
-
-      final Thread stdoutGobbler = new StreamGobbler(stdout, "STDOUT");
-      stdoutGobbler.start();
-
-      final int result = p.waitFor();
-      if (result != 0) {
-        throw new RuntimeException("Make BLAST DB failed.");
+      if (br.readLine() == null) {
+        fastaIsEmpty = true;
+      } else {
+        fastaIsEmpty = false;
       }
-    } catch (final IOException | InterruptedException e) {
-      this.logger.error("BLAST Failure", e);
-      throw new RuntimeException(e);
-    } finally {
-      p.destroy();
+    } catch (final IOException e) {
+      throw new RuntimeException("Unable to read FASTA file " + fastaPath);
     }
 
-    this.logger.info("Library written to {}", fastaPath.toAbsolutePath());
+    if (!fastaIsEmpty) {
+      final String[] cmd = {"makeblastdb",
+          "-dbtype",
+          "nucl",
+          "-parse_seqids",
+          "-in",
+          fastaPath.toAbsolutePath().toString(),
+          "-title",
+          libraryName,
+          "-out",
+          FileSystems.getDefault().getPath(this.outputDirectory.toString(), libraryName).toAbsolutePath().toString()};
+
+      final ProcessBuilder pb = new ProcessBuilder(cmd);
+
+      this.logger.info("Prepared command: {}", StringUtils.join(cmd, " "));
+
+      final Process p;
+
+      try {
+        p = pb.start();
+      } catch (final IOException e) {
+        throw new RuntimeException(e);
+      }
+
+      this.logger.debug("Started running library build.");
+      // Start the process
+      try (final InputStream error = p.getErrorStream(); final InputStream stdout = p.getInputStream()) {
+
+        // create listener for the stderr on separate thread (reports stderr to log).
+        final Thread errorGobbler = new StreamGobbler(error, "ERROR");
+        errorGobbler.start();
+
+        final Thread stdoutGobbler = new StreamGobbler(stdout, "STDOUT");
+        stdoutGobbler.start();
+
+        final int result = p.waitFor();
+        if (result != 0) {
+          throw new RuntimeException("Make BLAST DB failed.");
+        }
+      } catch (final IOException | InterruptedException e) {
+        this.logger.error("BLAST Failure", e);
+        throw new RuntimeException(e);
+      } finally {
+        p.destroy();
+      }
+
+      this.logger.info("Library written to {}", fastaPath.toAbsolutePath());
+    }
   }
 }
