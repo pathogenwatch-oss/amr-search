db2ce455671e766bfac6277a2117973549dd2f6f
Corin Yeats <corin.yeats@gmail.com>
2018-12-05 17:08:48 +0000

    Added missing parser.

--- paarsnp-lib/src/main/java/net/cgps/wgsa/paarsnp/core/models/variants/VariantJsonDeserializer.java
+++ paarsnp-lib/src/main/java/net/cgps/wgsa/paarsnp/core/models/variants/VariantJsonDeserializer.java
@@ -1,13 +1,13 @@
 package net.cgps.wgsa.paarsnp.core.models.variants;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import net.cgps.wgsa.paarsnp.core.models.variants.implementations.Frameshift;
 import net.cgps.wgsa.paarsnp.core.models.variants.implementations.PrematureStop;
+import net.cgps.wgsa.paarsnp.core.models.variants.implementations.PromoterMutation;
 import net.cgps.wgsa.paarsnp.core.models.variants.implementations.ResistanceMutation;
 
 import java.io.IOException;
@@ -15,12 +15,14 @@ import java.io.IOException;
 public class VariantJsonDeserializer extends JsonDeserializer {
 
   @Override
-  public Object deserialize(final JsonParser p, final DeserializationContext ctxt) throws IOException, JsonProcessingException {
+  public Object deserialize(final JsonParser p, final DeserializationContext ctxt) throws IOException {
     ObjectMapper mapper = (ObjectMapper) p.getCodec();
     ObjectNode root = mapper.readTree(p);
 
     if (root.has("type")) {
       return mapper.readValue(root.toString(), ResistanceMutation.class);
+    } else if (root.has("referenceLocation") && root.get("referenceLocation").asInt() < 0) {
+      return mapper.readValue(root.toString(), PromoterMutation.class);
     } else if ("truncated".equals(root.get("name").asText())) {
       return mapper.readValue(root.toString(), PrematureStop.class);
     } else {
