eecd1732a9782ca2ba149bc01dd708c011abea78
Corin Yeats <corin.yeats@gmail.com>
2019-10-29 09:33:46 +0000

    Added a test, but now changing plan completely.

---
+++ paarsnp-lib/src/test/java/net/cgps/wgsa/paarsnp/core/models/variants/implementations/ResistanceMutationTest.java
@@ -0,0 +1,10 @@
+package net.cgps.wgsa.paarsnp.core.models.variants.implementations;
+
+import org.junit.Test;
+
+public class ResistanceMutationTest {
+
+  @Test
+  public void isPresent() {
+  }
+}
