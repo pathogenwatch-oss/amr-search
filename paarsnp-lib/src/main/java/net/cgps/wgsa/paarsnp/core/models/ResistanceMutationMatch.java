92de24e004b8b1779f714018f62e5a3f605be39b
Corin Yeats <corin.yeats@gmail.com>
2019-11-08 16:51:55 +0000

    Added toString method for mutation match.

--- paarsnp-lib/src/main/java/net/cgps/wgsa/paarsnp/core/models/ResistanceMutationMatch.java
+++ paarsnp-lib/src/main/java/net/cgps/wgsa/paarsnp/core/models/ResistanceMutationMatch.java
@@ -4,6 +4,7 @@ import net.cgps.wgsa.paarsnp.core.lib.blast.Mutation;
 import net.cgps.wgsa.paarsnp.core.models.variants.Variant;
 
 import java.util.Collection;
+import java.util.Objects;
 
 public class ResistanceMutationMatch {
 
@@ -27,5 +28,27 @@ public class ResistanceMutationMatch {
 
   public Collection<Mutation> getCausalMutations() {
     return this.causalMutations;
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    final ResistanceMutationMatch that = (ResistanceMutationMatch) o;
+    return Objects.equals(resistanceMutation, that.resistanceMutation) &&
+        Objects.equals(causalMutations, that.causalMutations);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(resistanceMutation, causalMutations);
+  }
+
+  @Override
+  public String toString() {
+    return "ResistanceMutationMatch{" +
+        "resistanceMutation=" + resistanceMutation +
+        ", causalMutations=" + causalMutations +
+        '}';
   }
 }
