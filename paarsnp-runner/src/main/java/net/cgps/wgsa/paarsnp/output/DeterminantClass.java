d120d056e34d2b0a179f822ddb8c2e27896a0f9a
Corin Yeats <corin.yeats@cgps.group>
2021-04-16 17:20:14 +0100

    Extension

--- paarsnp-runner/src/main/java/net/cgps/wgsa/paarsnp/output/DeterminantClass.java
+++ paarsnp-runner/src/main/java/net/cgps/wgsa/paarsnp/output/DeterminantClass.java
@@ -3,13 +3,14 @@ package net.cgps.wgsa.paarsnp.output;
 import net.cgps.wgsa.paarsnp.core.models.ElementEffect;
 
 public enum DeterminantClass {
-  RESISTANCE, CONTRIBUTES, REDUCES, OTHER;
+  RESISTANCE, CONTRIBUTES, REDUCES, INDUCED, OTHER;
 
   public static DeterminantClass fromModifierEffect(final ElementEffect elementEffect) {
     switch (elementEffect) {
       case SUPPRESSES:
         return REDUCES;
       case INDUCED:
+        return INDUCED;
       case NONE:
         return OTHER;
     }
